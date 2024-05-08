188f5f44f049a448a0c3898cce17599cccd9aef4
Marcos Costa <79596734+MarcosCosta-dv@users.noreply.github.com>
2024-05-07 23:52:27 -0300

    Cp-Javascript

--- ex05/script05.js
+++ ex05/script05.js
@@ -1,1 +1,16 @@
 
+const tipoCarro = prompt("Qual tipo de carro você deseja comprar? (hatch, sedan, motocicleta ou caminhonete)");
+const tipoCarroLowerCase = tipoCarro.toLowerCase();
+switch (tipoCarroLowerCase) {
+    case "hatch":
+        alert("Compra efetuada com sucesso!");
+        break;
+    case "sedan":
+    case "motocicleta":
+    case "caminhonete":
+        alert("Tem certeza de que não prefere um modelo hatch?");
+        break;
+    default:
+        alert("Não trabalhamos com este tipo de automóvel aqui.");
+        break;
+}
